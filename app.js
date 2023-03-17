e22d531af4aa58f2c0a0207babdba74d3571b492
Rebecca Abel <rebeccaabel@hotmail.se>
2023-03-17 09:31:14 +0100

    reworked the logic bc bird animation

--- app.js
+++ app.js
@@ -1,4 +1,112 @@
-const canvas = document.getElementById('game-canvas');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+let frames = 0;
+
+const sprite = new Image();
+sprite.src = "sprite.png";
+
+const background = {
+    sX : 0,
+    sY : 0,
+    w : 275,
+    h : 226,
+    x : 0,
+    y : canvas.height - 226,
+    
+    draw : function(){
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+    
+}
+
+const ground = {
+    sX : 276,
+    sY : 0,
+    w : 224,
+    h : 112,
+    x : 0,
+    y : canvas.height - 112,
+
+    draw: function () {
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
+
+const bird = {
+    animation : [
+        {sourceX: 276, sourceY: 112},
+        {sourceX: 276, sourceY: 139},
+        {sourceX: 276, sourceY: 164},
+        {sourceX: 276, sourceY: 139},
+    ],
+    x: 50,
+    y: 150,
+    w: 34,
+    h: 26,
+
+    frame: 0,
+
+    draw: function () {
+        let birdAnimation = this.animation[this.frame];
+
+        context.drawImage(sprite, birdAnimation.sourceX, birdAnimation.sourceY, this.w, this.h, this.x - this.w/2, this.y - this.h/2, this.w, this.h);
+
+    }
+}
+
+const getReady = {
+    sX : 0,
+    sY : 228,
+    w : 173,
+    h : 152,
+    x : canvas.width/2 - 173/2,
+    y : 80,
+
+    draw : function(){
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+    }
+}
+
+
+function draw () {
+    context.fillStyle = "#70c5ce";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    background.draw();
+    ground.draw();
+    bird.draw();
+    getReady.draw();
+
+}
+
+/*function update () {
+
+}*/
+
+function loop () {
+   //update();
+    draw();
+    frames++;
+    requestAnimationFrame(loop);
+}
+
+loop();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 
 const birdImg = new Image();
@@ -36,10 +144,10 @@ document.body.onkeyup = function(e) {
     }
 }
 
-document.getElementById('restart-button').addEventListener('click', () => {
-hideEndMenu();
-resetGame();
-resetCtx();
+document.getElementById('restart-button').addEventListener('click', () => { // window reload 
+    resetGame();
+    hideEndMenu();
+    resetCtx();
 })
 
 
@@ -53,7 +161,7 @@ function scoreIncrease () {
             }
 
 
-            if (birdX < pipeX + PIPE_WIDTH) {
+         /*   if (birdX < pipeX + PIPE_WIDTH) {
                 scored = false; 
             }
 
@@ -149,7 +257,7 @@ function resetCtx () {
         return;
     }
 
-    pipeX -= 1.5;
+    pipeX -= 1.7;
     if(pipeX < -50) {
         pipeX = 400; 
         pipeY = Math.random() * (canvas.height - PIPE_GAP) + PIPE_WIDTH; 
@@ -165,5 +273,4 @@ function resetCtx () {
 
 }
 
-
-resetCtx();
+resetCtx();*/ 
